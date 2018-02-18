caf4a57afc04fcd78e97cc005cfdfa3af2652dee
rafi isaharov <isaharov@gmail.com>
2018-02-18 06:54:36 +0200

    device mangment

--- src/app/layout/devices/devices.module.ts
+++ src/app/layout/devices/devices.module.ts
@@ -1,15 +1,21 @@
 import { PageHeaderModule } from './../../shared/modules/page-header/page-header.module';
 import { NgModule } from '@angular/core';
+import { NgbModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { DevicesComponent } from './devices.component';
 import { DevicesRoutingModule } from './devices-routing.module';
+import { CollapseComponent } from './components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     DevicesRoutingModule, 
-    PageHeaderModule
+    PageHeaderModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  declarations: [DevicesComponent]
+  declarations: [DevicesComponent,CollapseComponent]
 })
 export class DevicesModule { }
